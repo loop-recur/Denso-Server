12a4e27cd4172a1d589740a244afb41f4615ec98
Brian <brian@trnsfr.com>
2012-01-01 09:26:37 -0800

    fix for cors

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -2,7 +2,26 @@ module Api
 class ApplicationController < ::ApplicationController
   layout false
   skip_before_filter :require_http_authentication
-  before_filter :authenticate_user!
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
+  
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+  
+  def cors_preflight_check
+    if request.method =~ /options/i
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, UserAgent'
+      headers['Access-Control-Max-Age'] = '1728000'
+
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
   
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Denso::Application.routes.draw do
   devise_for :users
 
-  
   namespace :admin do
     root :to => "apps#index"
     resources :apps
     resources :users
   end
   
-  namespace :api do
-    resources :apps
-    resources :yelps
-  end
+  match 'api/apps.json' => 'api/apps#index', :as => 'apps'
+  match 'api/yelps.json' => 'api/yelps#index', :as => 'yelps'
   
   root :to => "admin/apps#index"
 end
