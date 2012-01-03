aa1e69a6c5e8eb1084d8f74a70c0c93aab64d0ed
Brian <brian@trnsfr.com>
2012-01-03 10:30:14 -0800

    fake yelp

--- app/controllers/api/yelps_controller.rb
+++ app/controllers/api/yelps_controller.rb
@@ -3,6 +3,12 @@ module Api
 class YelpsController < ApplicationController
   
   def index
+    response = call_yelp rescue FakeYelp.response(params[:term])
+    render(:json => response)
+  end
+  
+private
+  def call_yelp
     client = Yelp::Client.new
     request = Yelp::V2::Search::Request::BoundingBox.new(:term => params[:term],
                                                          :sw_latitude => params[:sw_latitude],
@@ -14,8 +20,7 @@ class YelpsController < ApplicationController
                                                          :consumer_secret => 'wdFGjzGnQz11O6_Ehxqj1dkPnXA',
                                                          :token => 'edoyaJIsvBtJaefxXE4nHD3f7eOZJhXf',
                                                          :token_secret => 'QcQTiW-fO_2sUL70XHvemcxD9gs')
-    response = client.search(request)
-    render(:json => response)
+    client.search(request)
   end
 end
 
