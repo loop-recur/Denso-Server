b142d954ee755aab9823cf808c067edd1fcb4d20
Brian <brian@trnsfr.com>
2012-03-10 19:28:09 -0800

    few changes for case sensitive params

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -2,7 +2,7 @@ module Api
 
 class CategoriesController < ApplicationController
   def index
-    @apps = Category.find_by_name(params[:category]).apps.uniq
+    @apps = Category.by_name(params[:category]).first.apps.uniq
     render(:json => @apps.map(&:to_hash))
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :app_categories
   has_many :apps, :through => :app_categories
+  scope :by_name, proc {|name| where(["name LIKE ?", "%#{name}%"]) }
 end
