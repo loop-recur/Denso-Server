5d79083a1a3bbaa836b05e1ff769537ca646934e
ryan charette <ryan@looprecur.com>
2012-03-22 13:46:16 -0700

    Updated app hash for categories to have a category object property, also filter apks

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -2,8 +2,14 @@ module Api
 
 class CategoriesController < ApplicationController
   def index
-    @apps = Category.by_name(params[:category]).first.apps.uniq
-    render(:json => @apps.map(&:to_hash))
+    unless params[:apks]
+      @apps = Category.by_name(params[:category]).first.apps.uniq
+    else
+      @apps = Category.by_name(params[:category]).first.apps.select {|a| a.package_file_name =~ /.*\.zip$/}.uniq
+    end
+     
+    hashes = @apps.map(&:to_hash).map {|a| a.merge(:category => params[:category].downcase)}
+    render(:json => hashes)
   end
 end
 
